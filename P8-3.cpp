223264cd2980b77aa84b75ba4f2cf2aed01cc543
braylonrieman14 <b-rieman.2@onu.edu>
2024-10-24 13:47:32 -0400

    completed romanCharValue function

--- P8-3.cpp
+++ P8-3.cpp
@@ -24,5 +24,32 @@ int main(void)
 
 int romanCharValue(char r)
 {
-	
+	int count = 0;
+
+	switch (r)
+	{
+	case 'I':
+		count += 1;
+		break;
+	case 'V':
+		count += 5;
+		break;
+	case 'X':
+		count += 10;
+		break;
+	case 'L':
+		count += 50;
+		break;
+	case 'C':
+		count += 100;
+		break;
+	case 'D':
+		count += 500;
+		break;
+	case 'M':
+		count += 1000;
+		break;
+	}
+
+	return(count);
 }
