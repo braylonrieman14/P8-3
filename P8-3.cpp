ccbb877b57a3e6d89d2afe2af774094f52d43e95
braylonrieman14 <b-rieman.2@onu.edu>
2024-10-24 13:47:11 -0400

    Add project files.

---
+++ P8-3.cpp
@@ -0,0 +1,28 @@
+/*
+* File: P8-3.cpp
+* Author : Braylon Rieman
+* Date : 10 / 24 / 2024
+*
+* Description : Roman Numeral Converter
+*
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//function prototypes
+int romanCharValue(char r);
+
+int main(void)
+{
+	int value;
+	value = romanCharValue('I');
+
+	cout << " = " << value;//hello
+}
+
+int romanCharValue(char r)
+{
+	
+}
