d2d358fb76bcddc363afa60a89db93871a15d109
braylonrieman14 <b-rieman.2@onu.edu>
2024-10-24 14:08:17 -0400

    Finished P8-3

---
+++ P8-3(1).cpp
@@ -0,0 +1,25 @@
+/*
+* File: P8-3.cpp
+* Author : Braylon Rieman
+* Date : 10 / 24 / 2024
+*
+* Description : Roman Numeral Converter
+*
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//function prototypes
+int romanCharValue(char r);
+
+int main(void)
+{
+
+}
+
+int romanCharValue(char r)
+{
+
+}
--- P8-3.cpp
+++ P8-3.cpp
@@ -12,44 +12,81 @@
 using namespace std;
 
 //function prototypes
+int convertRomanToInt(string s);
 int romanCharValue(char r);
 
 int main(void)
 {
-	int value;
-	value = romanCharValue('I');
+	//variables
+	string input;
+	
+	while (true)
+	{
+		cout << "Enter Roman number or Q to quit: ";
+		cin >> input;
 
-	cout << " = " << value;//hello
+		if (input == "Q")
+			break;
+
+		cout << convertRomanToInt(input) << endl;
+	}
+	
+	int value;
+	value = convertRomanToInt("II");
+
+	cout << " = " << value;
 }
 
 int romanCharValue(char r)
 {
-	int count = 0;
-
+	int result = 0;
 	switch (r)
 	{
 	case 'I':
-		count += 1;
+		result = 1;
 		break;
 	case 'V':
-		count += 5;
+		result = 5;
 		break;
 	case 'X':
-		count += 10;
+		result = 10;
 		break;
 	case 'L':
-		count += 50;
+		result = 50;
 		break;
 	case 'C':
-		count += 100;
+		result = 100;
 		break;
 	case 'D':
-		count += 500;
+		result = 500;
 		break;
 	case 'M':
-		count += 1000;
+		result = 1000;
 		break;
 	}
+	return(result);
+}
 
+int convertRomanToInt(string s)
+{
+	int count = 0;
+	int currentChar;
+	int nextChar;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		currentChar = romanCharValue(s[i]);
+		nextChar = romanCharValue(s[i+1]);
+
+		if (currentChar >= nextChar || s.length() == 1)
+		{
+			count += currentChar;
+		}
+		else
+		{
+			count += (nextChar - currentChar);
+			i++;
+		}
+	}
 	return(count);
 }
